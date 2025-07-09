3b62b003f5bcd49747befbdcfa40cfdee105e813
Rivera Angel <159390895+RiveraAngeldev@users.noreply.github.com>
2025-07-09 10:39:15 -0400

    Delete src directory

--- src/Utils/Handler.cpp
+++
@@ -1,6 +0,0 @@
-#include "Handler.h"
-
-// Initialize the static variables
-ofRectangle Handler::HUD_SCREEN = ofRectangle(0, 0, Handler::SECTION_WIDTH, Handler::HUD_HEIGHT);
-ofRectangle Handler::GAME_SCREEN = ofRectangle(0, Handler::HUD_HEIGHT, Handler::SECTION_WIDTH, Handler::SECTION_HEIGHT);
-bool Handler::DEBUG = false;
